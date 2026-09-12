fbb90d779b0935341c52daed4affbfd6641d0503
Santiago <santiago.morales@utec.edu.pe>
2026-09-12 00:10:42 -0500

    fix: created and configura .dockerignore, and modificate vite.config.ts because configure environment variables connection

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,10 @@ export default defineConfig(({ mode }) => {
     plugins: [react(),tailwindcss(),],
     server: {
       proxy: {
-        '/api/auth': { target: env.VITE_IDENTITY_API_URL || 'http://localhost:8001', changeOrigin: true },
-        '/api/products': { target: env.VITE_CATALOG_API_URL || 'http://localhost:8002', changeOrigin: true },
-        '/api/orders': { target: env.VITE_ORDER_API_URL || 'http://localhost:8003', changeOrigin: true },
-        '/api/analytics': { target: env.VITE_ANALYTICS_API_URL || 'http://localhost:8005', changeOrigin: true },
+        '/api/auth': { target: env.VITE_IDENTITY_API_URL, changeOrigin: true },
+        '/api/products': { target: env.VITE_CATALOG_API_URL,changeOrigin: true },
+        '/api/orders': { target: env.VITE_ORDER_API_URL, changeOrigin: true },
+        '/api/analytics': { target: env.VITE_ANALYTICS_API_URL, changeOrigin: true },
       },
     },
   }
